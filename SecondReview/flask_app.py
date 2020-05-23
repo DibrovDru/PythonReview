de43d7050013040b8b5836ebc04b4e0a5b9db721
DibrovDru <dibrov.aa@phystech.edu>
2020-05-23 14:39:54 +0300

    some changes and unit tests

--- SecondReview/flask_app.py
+++ SecondReview/flask_app.py
@@ -4,10 +4,10 @@ from lxml import etree
 import requests
 
 
-def parseBookXML(xml_file):
-    ffw = []
-    f = open(xml_file, 'r')
-    root = etree.parse(f)
+def parseStatisticsXML(xml_file):
+    data_list = []
+    file = open(xml_file, 'r')
+    root = etree.parse(file)
 
     for valute in root.getroot().getchildren():
         temp_nominal = []
@@ -17,17 +17,17 @@ def parseBookXML(xml_file):
             else:
                 temp_nominal.append(symbols)
 
-        ffw.append(valute[3].text + ' ' + str(float(''.join(temp_nominal)) / float(valute[2].text)))
+        data_list.append(valute[3].text + ' ' + str(float(''.join(temp_nominal)) / float(valute[2].text)))
 
-    return ffw
+    return data_list
 
 app = Flask(__name__)
-f = open('valute_xml', 'wb')
+file = open('valute_xml', 'wb')
 ufr = requests.get("http://www.cbr.ru/scripts/XML_daily_eng.asp")
-f.write(ufr.content)
-f.close()
+file.write(ufr.content)
+file.close()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='tempCourse', valute_list=parseBookXML('valute_xml'))
+    return render_template('index.html', title='tempCourse', valute_list=parseStatisticsXML('valute_xml'))
