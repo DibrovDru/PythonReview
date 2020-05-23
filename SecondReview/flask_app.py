6fc6c3370662db9713e631f431a07c7f520dd7fa
DibrovDru <dibrov.aa@phystech.edu>
2020-05-23 16:42:17 +0300

    some changes

--- SecondReview/flask_app.py
+++ SecondReview/flask_app.py
@@ -6,26 +6,19 @@ import requests
 
 def parseStatisticsXML(xml_file):
     data_list = []
-    file = open(xml_file, 'r')
-    root = etree.parse(file)
+    with open(xml_file) as file:
+        root = etree.parse(file)
 
     for valute in root.getroot().getchildren():
-        temp_nominal = []
-        for symbols in valute[4].text:
-            if symbols == ',':
-                temp_nominal.append('.')
-            else:
-                temp_nominal.append(symbols)
-
+        temp_nominal = valute[4].text.replace(',', '.')
         data_list.append(valute[3].text + ' ' + str(float(''.join(temp_nominal)) / float(valute[2].text)))
 
     return data_list
 
 app = Flask(__name__)
-file = open('valute_xml', 'wb')
 ufr = requests.get("http://www.cbr.ru/scripts/XML_daily_eng.asp")
-file.write(ufr.content)
-file.close()
+with open('valute_xml', 'wb') as file:
+    file.write(ufr.content)
 
 @app.route('/')
 @app.route('/index')
